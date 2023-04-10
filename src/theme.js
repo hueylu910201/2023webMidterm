5d1a15f156c712a920e2003f0846081bf8ba0fef
hueylu910201 <hueylu910201@gmail.com>
2023-04-11 03:26:21 +0800

    fix-NotificationBgColor

--- src/theme.js
+++ src/theme.js
@@ -14,7 +14,7 @@ const darkTheme = {
     colorInformation:"#636363",
     colorList:"#3a3b3b",
     colorButtonTop:'#494c4d',
-    colorNotice:"#494c4d"
+    colorNotice:"#7a8082"
   },
   components: {
     Button: {
